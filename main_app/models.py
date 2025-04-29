6fe3ab89e649beddd5f2a713e13a5adb61b46c6a
NargessAlmahdi <nargess.almahdi2002@gmail.com>
2025-04-29 19:36:23 +0300

    typeface list

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Typeface(models.Model):
+    name = models.CharField(max_length=100)
+    designer = models.CharField(max_length=100)
+    classification = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='typefaces/')
+    link = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def typeface_index(request):
+    return render(request, 'typefaces/index.html', {'typefaces': typefaces})
+
+
+class Typeface:
+    def __init__(self, name, designer, classification, image, link):
+        self.name = name
+        self.designer = designer
+        self.classification = classification
+        self.image = image
+        self.link = link
+
+typefaces = [
+    Typeface('Helvetica', 'Sammy Mark', 'Sans Serif', 'Helvetica.png', 'https://example.com/Helvetica'),
+    Typeface('GESS', 'Steve Mark', 'Sans Serif', 'GESS.png', 'https://example.com/GESS'),
+    Typeface('Futura', 'Donald Alex', 'Sans Serif', 'Futura.png', 'https://example.com/Futura'),
+    Typeface('Proxima Nova', 'Lexy Hamilton', 'Sans Serif', 'Proxima.png', 'https://example.com/Proxima')
+]
